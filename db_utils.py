d3d5d7511adbc63ba322e25dfce1ea39002f516c
alexaib <alexaib@outlook.es>
2023-02-14 11:28:54 +0100

    Implemented all db_utils functions

--- db_utils.py
+++ db_utils.py
@@ -1,82 +1,123 @@
+import sys
+
 from class_defs import *
 from psycopg2.extensions import connection
 import psycopg2 as db_driver
+import psycopg2.errors as db_errors
 
 db_conn: connection = None
 
+
 # Init functions
 def init_connection() -> None:
-	global db_conn
-	db_conn = db_driver.connect(
-		database = "gym_db",
-		host = "localhost",
-		user = "postgres",
-		password = "admin123",
-		port = "25432",
-	)
-	assert db_conn
-	while db_conn.closed:
-		from time import sleep
-		secs = 3
-		print(f"La conexion no se pudo establecer, reintentando en {secs} segundos")
-		sleep(secs)
-	print("Conexion establecida correctamente")
+    global db_conn
+    db_conn = db_driver.connect(
+        database="gym_db",
+        host="localhost",
+        user="postgres",
+        password="admin123",
+        port="25432",
+    )
+    assert db_conn
+    while db_conn.closed:
+        from time import sleep
+        secs = 3
+        print(f"La conexion no se pudo establecer, reintentando en {secs} segundos")
+        sleep(secs)
+    print("Conexion establecida correctamente")
+
+
+def recreate_db_structs(sql_script: str) -> None:
+    global db_conn
+    print("Intentando recrear la estructura estandar de la BBDD...")
+    with db_conn.cursor() as cursor:
+        cursor.execute(sql_script)
+        cursor.close()
+
 
 def teardown_connection() -> None:
-	global db_conn
-	db_conn.commit()
-	print("Modificaciones almacenadas correctamente")
-	db_conn.close()
-	print("Conexion cerrada correctamente")
+    global db_conn
+    db_conn.commit()
+    print("Modificaciones almacenadas correctamente")
+    db_conn.close()
+    print("Conexion cerrada correctamente")
+
 
 def populate_sports() -> None:
-	global db_conn
-	with db_conn.cursor() as cursor:
-		cursor.execute("SELECT * FROM public.clients ORDER BY nid ASC")
-		if len(cursor.fetchall()):
-			return # Return if sports is already populated
-		print("Inicializando deportes...")
-		for sport, price in avail_sports.items():
-			cursor.execute("INSERT INTO public.sports(name, price) VALUES (%s, %s);",
-			(sport, price))
-			print(sport, price)
-		cursor.close()
+    global db_conn
+    with db_conn.cursor() as cursor:
+        cursor.execute("SELECT * FROM public.sports ORDER BY name ASC")
+        if len(cursor.fetchall()) > 0: return  # Return if sports is already populated
+        print("Inicializando deportes...")
+        for sport, price in avail_sports.items():
+            cursor.execute("INSERT INTO public.sports(name, price) VALUES (%s, %s);",
+                           (sport, price))
+            print(sport, price)
+        cursor.close()
+
 
 # Exposed functions
 def add_client(client: Client) -> None:
-	global db_conn
-	with db_conn.cursor() as cursor:
-		cursor.execute(
-			"INSERT INTO public.clients(nid, name, birth_date, phone) VALUES (%s, %s, %s, %s);",
-			(client.id, client.name, client.birth_date, client.phone))
-		cursor.close()
+    global db_conn
+    with db_conn.cursor() as cursor:
+        cursor.execute(
+            "INSERT INTO public.clients(nid, name, birth_date, phone) VALUES (%s, %s, %s, %s);",
+            (client.id, client.name, client.birth_date, client.phone))
+        cursor.close()
+
 
 def del_client(id: str) -> None:
-	global db_conn
-	with db_conn.cursor() as cursor:
-		cursor.execute(
-			"DELETE FROM public.clients WHERE nid LIKE %s;",
-			(id))
-		cursor.close()
+    global db_conn
+    with db_conn.cursor() as cursor:
+        cursor.execute(
+            "DELETE FROM public.clients WHERE nid LIKE %s;",
+            (id))
+        cursor.close()
+
 
 def show_client(id: str = "") -> None:
-	global db_conn
-	with db_conn.cursor() as cursor:
-		if not id or id.isspace():
-			cursor.execute("SELECT * FROM public.clients ORDER BY nid ASC")
-		else:
-			cursor.execute("SELECT * FROM public.clients WHERE nid LIKE %s", (id,))
-		for row in cursor.fetchall():
-			Client(*row).print_data_pretty()
+    global db_conn
+    with db_conn.cursor() as cursor:
+        if not id or id.isspace():
+            cursor.execute("SELECT * FROM public.clients ORDER BY nid ASC")
+        else:
+            cursor.execute("SELECT * FROM public.clients WHERE nid LIKE %s", (id,))
+        for row in cursor.fetchall():
+            Client(*row).print_data_pretty()
 
-def add_client_to_sport(id: str, sport_name: str) -> None:
-	global db_conn
-	pass
+
+def add_client_to_sport(id: str, sport_name: str, sport_time: str) -> None:
+    global db_conn
+    try:
+        with db_conn.cursor() as cursor:
+            cursor.execute(
+                "INSERT INTO public.clients_sports(client_nid, sport_name, sport_time) VALUES (%s, %s, %s);", (id, sport_name, sport_time))
+            cursor.close()
+    except db_errors.ForeignKeyViolation:
+        print("El deporte/cliente no existe en su respectiva tabla", file=sys.stderr)
+
 
 def del_client_from_sport(id: str, sport_name: str) -> None:
-	global db_conn
-	pass
+    global db_conn
+    try:
+        with db_conn.cursor() as cursor:
+            cursor.execute(
+                "DELETE FROM public.clients_sports WHERE client_nid LIKE %s AND sport_name LIKE %s;",
+                (id, sport_name))
+            cursor.close()
+    except db_errors.ForeignKeyViolation:
+        print("El deporte/cliente no existe en su respectiva tabla", file=sys.stderr)
+
 
 def show_client_sports(id: str) -> None:
-	global db_conn
-	pass
+    global db_conn
+    with db_conn.cursor() as cursor:
+        cursor.execute(
+            "SELECT sport_name, sport_time FROM public.clients_sports WHERE client_nid LIKE %s", (id,))
+        res = cursor.fetchall()
+        for row in res:
+            for cell in row:
+                print(cell)
+        if not len(res):
+            print("No se ha seleccionado ningun cliente", file=sys.stderr)
+        cursor.close()
