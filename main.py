209752db588e3f6ffbd1e9def19264a1c6e342ad
alexaib <alexaib@outlook.es>
2023-02-14 11:56:39 +0100

    DB initialization on first run

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
+import sys
+
 from menu_utils import menu
-from db_utils import init_connection, teardown_connection, populate_sports
+from db_utils import init_connection, teardown_connection
 
 
 def main() -> None:
     init_connection()
-    populate_sports()
+    init_db()
     menu()
+
+
+def init_db() -> None:
+    from db_utils import recreate_db_structs, populate_sports
+    try:
+        script = open("sql/table_defs.sql", 'r').read()
+        recreate_db_structs(script)
+    except FileNotFoundError:
+        print("No se ha encontrado el script SQL de creacion de tablas", file=sys.stderr)
+    populate_sports()
+
 
 if __name__ == '__main__':
     try:
